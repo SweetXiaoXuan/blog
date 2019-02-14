04aa0b644b3396a5d7b079f21a7d368e5eaff74a
xurunfei <=1194289919@qq.com>
2019-02-14 11:27:25 +0800

    fix(core): 1.页面跳转demo

--- feel/src/main/java/com/mf/feel/controller/DemoController.java
+++ feel/src/main/java/com/mf/feel/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.mf.feel.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/demo")
@@ -11,5 +12,13 @@ public class DemoController {
     @GetMapping
     public String test() {
         return "test";
+    }
+
+    /**
+     * 页面跳转 demo
+     */
+    @GetMapping("demo")
+    public ModelAndView demo() {
+        return new ModelAndView("demo.html");
     }
 }
